5a32572d9561cb16f2bc1c1de84e268a11f3da78
Ansh Darji <anshdarji96@gmail.com>
2024-05-20 10:41:26 +0530

    requirement added

---
+++ README.md
@@ -0,0 +1,13 @@
+## how to run
+'''bash
+conda create -n sinlang python ==3.7
+'''
+
+
+'''bash
+conda activate signlang
+'''
+
+'''bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ='SignLanguage',
+    version = '0.0.0',
+    author= 'Ansh',
+    author_email = "anshdarji@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
+
